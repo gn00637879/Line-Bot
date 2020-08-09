12767943186ebc9f40df3daf2f84a5d1fd37bed4
Rex Chang <gn00637879@gmail.com>
2020-08-09 23:39:43 +0800

    update6

--- app.py
+++ app.py
@@ -38,10 +38,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	msg = event.message.text
-	s = '吃了嗎'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text='吃飯了'))
 
 
 if __name__ == "__main__":
