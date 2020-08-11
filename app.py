8755cc83ea52ffb3b83c309c5cd24ad31102b589
Rex Chang <gn00637879@gmail.com>
2020-08-11 21:27:47 +0800

    stiker

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             sticker_message)
-            return
+        return
 
     if msg in ['Hi' , 'hi']:
         r = '嗨'
